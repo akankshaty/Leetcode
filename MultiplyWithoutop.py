47717ef8c89c1b984415601fe213d08b23dd27be
Akanksha Tyagi <akankshatyagi18@gmail.com>
2018-04-09 20:36:12 -0700

    strStr

--- MultiplyWithoutop.py
+++ MultiplyWithoutop.py
@@ -2,7 +2,7 @@
 
 
 #Multiply two positive integers without using * operator
-
+'''
 def helper(small,big):
     if small == 0:
         return 0
@@ -20,13 +20,43 @@ def multiply(a,b):
     small = b if a > b else a
     big = a if a > b else b
     return helper(small,big)
-
+'''
 #Code to check the above function
-ans = multiply(32,32)
-print(ans)
+#ans = multiply(32,32)
+#print(ans)
+
+pathLength = 9
+l = [[1,10],[1,6],[2,8],[3,5]]
+
+class Solution(object):
+    def merge(self,pathLength, intervals):
+        """
+        :type intervals: List[Interval]
+        :rtype: List[Interval]
+        """
+        count = 0
+        flag = 0
+        if len(intervals) == 0: return []
+        intervals = sorted(intervals, key = lambda x: x[0])
+        res = [intervals[0]]
+        
+        for n in intervals[1:]:
+            if n[1] > pathLength:
+                flag = 1
+            if n[0] <= res[-1][1] and n[0] <= pathLength: 
+                res[-1][1] = max(n[1], res[-1][1])
+                count += 1            
+            else: res.append(n)
+            print(count)
+
+        if flag == 0:
+            return -1
+
+        return count
 
 
 
 
-
-    
+a = Solution()
+r = a.merge(pathLength,l)
+print(r)    
